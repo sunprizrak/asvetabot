993fea820d94acb7d020f215c3c325910ea93bd0
sunprizrak <sunprizrak@gmail.com>
2023-12-08 17:24:50 +0300

    ...

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ async def on_startup():
         url=f'{BASE_WEBHOOK_URL}{WEBHOOK_PATH}',
         secret_token=WEBHOOK_SECRET,
         allowed_updates=["message", "callback_query"],
+        certificate=open('/app/ssl/fullchain.pem', 'rb'),
     )
 
 
